edf2bcd0f66c1aeb0f59e30be0363d7d7e55a00a
MohammadRezaa <dreza703@gmail.com>
2024-05-28 15:37:49 +0330

    setting login and register page ui

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { RouterProvider } from "react-router-dom"
+import Login from "./features/identity/components/login"
+import Register from "./features/identity/components/register"
+import router from "./router"
+
+function App() {
+
+
+  return (
+    
+    <RouterProvider>
+    
+  )
+}
+
+export default App
